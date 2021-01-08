508f65d404564e6ce1d2d22ecb0375413b571113
tor4z <vwenjie@hotmail.com>
2021-01-08 16:14:01 +0800

    Fix known bugs

---
+++ mlutils/imread.py
@@ -0,0 +1,42 @@
+from libtiff import TIFF
+import imageio
+import cv2
+import numpy as np
+
+
+def read_gif(filename):
+    im = imageio.mimread(filename)
+    im = np.array(im)
+    return im
+
+
+def read_tif(filename):
+    tif = TIFF.open(filename)
+    imgs = []
+    for img in tif.iter_images():
+        imgs.append(img)
+    return np.array(imgs)
+
+
+def read_jpg(filename):
+    img = cv2.imread(filename)
+    return img[:, :, ::-1]
+
+
+def read_png(filename):
+    return read_jpg(filename)
+
+
+def imread(filename):
+    suffix = filename.split('.')[-1]
+    if suffix == 'gif':
+        image = read_gif(filename)
+    elif suffix == 'tif':
+        image = read_tif(filename)
+    elif suffix == 'jpg' or suffix == 'jpeg':
+        image = read_jpg(filename)
+    elif suffix == 'png':
+        image = read_png(filename)
+    else:
+        raise NotImplementedError('Not support read {}'.format(filename))
+    return image
